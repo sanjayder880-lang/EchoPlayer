1fb1aa1d318c38d3c511d42ee8b508309ef70337
sanjayder880-lang <sanjayder880@gmail.com>
2026-01-24 10:52:01 +0530

    add assets

---
+++ script.js
@@ -0,0 +1,14 @@
+const songs = [
+    {
+        title : "Nokia",
+        artist: "nokia",
+        src: "C:\Users\sanjay\OneDrive\Documents\projects\EchoPlayer\assets\assets.songs\original_nokia_tune.mp3"
+        cover:"C:\Users\sanjay\OneDrive\Documents\projects\EchoPlayer\assets\assets.songs\original_nokia_tune.mp3"
+    },
+    {
+        title : "Transformers",
+        artist : "Steve Jablonsky",
+        src : "C:\Users\sanjay\OneDrive\Documents\projects\EchoPlayer\assets\assets.covers\nokia.jpg"
+        cover : "C:\Users\sanjay\OneDrive\Documents\projects\EchoPlayer\assets\assets.covers\transformers.jpg"
+    }
+]
